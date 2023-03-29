f8460b92438b669ab21215896d86100e5dde26d2
Nikhil Jain <nik.jain@gmail.com>
2023-03-29 16:29:08 +0530

    rearrange test code

--- tests/MessageTest.php
+++ tests/MessageTest.php
@@ -2,14 +2,47 @@
 
 use Gyaaniguy\Upworktest\Helpers\Enums;
 use Gyaaniguy\Upworktest\Model\Message;
+use Gyaaniguy\Upworktest\Model\Users\AbstractUser;
 use Gyaaniguy\Upworktest\Model\Users\Guardians\ParentOfStudent;
 use Gyaaniguy\Upworktest\Model\Users\Guardians\Teacher;
 use Gyaaniguy\Upworktest\Model\Users\Student;
-use Gyaaniguy\Upworktest\Model\Users\AbstractUser;
 use PHPUnit\Framework\TestCase;
 
 final class MessageTest extends TestCase
 {
+
+    /**
+     * @return Teacher
+     */
+    public function _getTeacher(): Teacher
+    {
+        return new Teacher(1, 'Teacher', 'Jain', 'teacher@gmail.com');
+    }
+
+
+    /**
+     * @return Student
+     */
+    private function _getStudent(): Student
+    {
+        return new Student(1, 'Nikhil', 'Jain', 'nik.jain@gmail.com');
+    }
+
+    /**
+     * @return ParentOfStudent
+     */
+    public function _getParentOfStudent(): ParentOfStudent
+    {
+        return new ParentOfStudent(1, 'father name', 'Jain', 'father@gmail.com');
+    }
+
+    public function _makeMessage(
+        AbstractUser $parentOfStudent,
+        AbstractUser $student,
+        string $messageText = 'My child!'
+    ): Message {
+        return new Message($parentOfStudent, $student, $messageText, Enums::MESSAGE_TYPE_MANUAL);
+    }
 
     public function testParentMessaging()
     {
@@ -41,27 +74,6 @@ final class MessageTest extends TestCase
         }
     }
 
-    /**
-     * @return Student
-     */
-    private function _getStudent(): Student
-    {
-        return new Student(1, 'Nikhil', 'Jain', 'nik.jain@gmail.com');
-    }
-
-    /**
-     * @return ParentOfStudent
-     */
-    public function _getParentOfStudent(): ParentOfStudent
-    {
-        return new ParentOfStudent(1, 'father name', 'Jain', 'father@gmail.com');
-    }
-
-    public function _makeMessage(AbstractUser $parentOfStudent, AbstractUser $student, string $messageText = 'My child!'): Message
-    {
-        return new Message($parentOfStudent, $student, $messageText, Enums::MESSAGE_TYPE_MANUAL);
-    }
-
     public function testStudentMessaging()
     {
         $student = $this->_getStudent();
@@ -69,7 +81,7 @@ final class MessageTest extends TestCase
         $teacher = $this->_getTeacher();
         $message = $this->_makeMessage($student, $teacher);
         $this->assertTrue($message->save());
-        
+
         // Parent should fail to send system message
         $message->setMessageType(Enums::MESSAGE_TYPE_SYSTEM);
         try {
@@ -90,7 +102,6 @@ final class MessageTest extends TestCase
         } catch (Exception $e) {
             self::assertStringContainsStringIgnoringCase('Message is invalid', $e->getMessage());
         }
-
     }
 
     public function testTeacherMessaging()
@@ -117,13 +128,6 @@ final class MessageTest extends TestCase
         $this->assertTrue($message->save());
     }
 
-    /**
-     * @return Teacher
-     */
-    public function _getTeacher(): Teacher
-    {
-        return new Teacher(1, 'Teacher', 'Jain', 'teacher@gmail.com');
-    }
 
     public function testFullName()
     {
